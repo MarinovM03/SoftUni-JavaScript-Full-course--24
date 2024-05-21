fa7398d1577a0a08bbeb5858b9d42726c961ac13
Martin Marinov <159044101+MarinovM03@users.noreply.github.com>
2024-05-21 20:08:07 +0100

    Learned how to use arrays
    Arrays

---
+++ array.js
@@ -0,0 +1,10 @@
+function array() {
+    let numArr = [10, 20, 30, 40];
+
+    console.log(numArr.length);
+    console.log(numArr[0]);
+    console.log(numArr[2]);
+    console.log(numArr[4]);
+}
+
+array();
