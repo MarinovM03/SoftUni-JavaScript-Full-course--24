bb9f3bde4fe34c651d843742795f765290bb1d79
Martin Marinov <159044101+MarinovM03@users.noreply.github.com>
2024-05-23 20:05:18 +0100

    Greetings by name
    Greetings by name

---
+++ greetingByName.js
@@ -0,0 +1,11 @@
+function greetingByName(input) {
+
+    let name = input[0];
+    let result = "Hello, " + name + "!";
+
+    console.log(result);
+}
+
+greetingByName(["Martin"]);
+greetingByName(["Boris"]);
+greetingByName(["John"]);
