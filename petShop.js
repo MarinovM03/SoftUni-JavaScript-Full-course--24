3a3696ec908ab9d964568d8b0fcdf075e0f16f41
Martin Marinov <159044101+MarinovM03@users.noreply.github.com>
2024-05-25 00:12:50 +0100

    Pet shop
    Pet shop exercise

---
+++ petShop.js
@@ -0,0 +1,11 @@
+function petShop(input) {
+    let dog = (input[0]);
+    let cat = (input[1]);
+
+    let result = dog * 2.50;
+    let result2 = cat * 4;
+
+    console.log(result + result2)
+}
+
+petShop([5, 4]);
