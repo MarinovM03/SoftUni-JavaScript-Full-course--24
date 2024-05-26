00e2321a8c0966325cb07759762ab20bbbb35574
Martin Marinov <159044101+MarinovM03@users.noreply.github.com>
2024-05-26 04:10:00 +0100

    Concatenate Data
    Learned how to concatenate

---
+++ concatenateData.js
@@ -0,0 +1,14 @@
+function concatenateData(input) {
+    
+    let firstName = input[0];
+    let lastName = input[1];
+    let age = Number(input[2]);
+    let town = input[3];
+
+   // let result = "You are " + firstName + " " + lastName + ", a " + age + "-years old person from " + town + ".";
+    let result = `You are ${firstName} ${lastName}, a ${age}-years old person from ${town}.`
+
+    console.log(result)
+}
+
+concatenateData(["Martin", "Marinov", 20, "Plovdiv"]);
