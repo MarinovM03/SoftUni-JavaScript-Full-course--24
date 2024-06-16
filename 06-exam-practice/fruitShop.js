3536a887e78c364f089b23fb202c19325e6a17f8
Martin Marinov <159044101+MarinovM03@users.noreply.github.com>
2024-06-16 01:27:54 +0100

    Fruit Shop
    Fruit Shop exercise.

---
+++ 06-exam-practice/fruitShop.js
@@ -0,0 +1,10 @@
+function fruitShop(input) {
+    let product = input[0];
+    let day = input[1];
+    let quantity = Number(input[2]);
+    let price = 0;
+
+
+}
+
+fruitShop(["apple","Tuesday","2"]);
