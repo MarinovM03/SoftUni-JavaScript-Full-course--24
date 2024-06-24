1435ce10fbbe71f6c88286fb95b1b2d7211a5531
Martin Marinov <159044101+MarinovM03@users.noreply.github.com>
2024-06-24 20:11:17 +0100

    True and False
    True and False exercise.

---
+++ 08-more-practice-knowledge/trueFalse.js
@@ -0,0 +1,22 @@
+function trueFalse(input) {
+
+    let a = 6;
+    let b = 6;
+    let isValid = false;
+
+    console.log(isValid);
+
+}
+
+trueFalse();
+
+function trueFalse1(input) {
+
+    let a = 5;
+    let isPositive = a > 0;
+
+    console.log(isPositive);
+
+}
+
+trueFalse1();
