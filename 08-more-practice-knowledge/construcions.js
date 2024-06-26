4f04689fe81767974fe0a81cd6b905792451b128
Martin Marinov <159044101+MarinovM03@users.noreply.github.com>
2024-06-26 21:33:58 +0100

    Constructions
    Constructions exercise.

---
+++ 08-more-practice-knowledge/construcions.js
@@ -0,0 +1,12 @@
+function osnovniKonstrukcii(input) {
+
+    let a = Number(input[0]);
+    let b = Number(input[1]);
+
+    if (a > b) {
+        console.log(true);
+    } 
+
+}
+
+osnovniKonstrukcii([6,6]);
