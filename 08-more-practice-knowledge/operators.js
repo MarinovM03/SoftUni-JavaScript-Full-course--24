9e7f32ac15d67924a6a29d31a298241415018f67
Martin Marinov <159044101+MarinovM03@users.noreply.github.com>
2024-06-28 23:53:27 +0100

    Operators
    Learned what the simple operators are.

---
+++ 08-more-practice-knowledge/operators.js
@@ -0,0 +1,28 @@
+function solve(input) {
+    let a = 5;
+    let b = "5";
+
+    console.log(a === b);
+}
+
+solve();
+
+function solve(input){
+    let a = "Martin";
+    let b = "Martin";
+
+    console.log(a === b);
+
+}
+
+solve();
+
+
+// == - compares by value.
+// === - compares by value and type.
+// != - looks for difference in value (if different will output TRUE)
+// !== - looks for difference in value and type (if different will output TRUE)
+// < - check if smaller
+// > - checks if bigger
+// <= - checks if smaller and equal
+// >= - checks if bigger and equal
