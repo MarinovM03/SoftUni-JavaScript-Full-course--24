a9d94b413a887e79fb5b9b288575795e91915cb4
Martin Marinov <159044101+MarinovM03@users.noreply.github.com>
2024-06-28 23:48:54 +0100

    Life of Variables
    Life of Variables exercise.

---
+++ 08-more-practice-knowledge/lifeOfVariables.js
@@ -0,0 +1,14 @@
+function jivotNaPromenlivite() {
+
+    if (true) {
+        console.log("work");
+        let test = 5;
+
+        console.log(test);
+    }
+
+    console.log(test);
+
+}
+
+jivotNaPromenlivite();
