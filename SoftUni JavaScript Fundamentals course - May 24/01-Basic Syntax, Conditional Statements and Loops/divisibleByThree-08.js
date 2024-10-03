3bbcd9bc6ac3016a6f1e16fc01492752849c9aef
Martin Marinov <159044101+MarinovM03@users.noreply.github.com>
2024-10-03 20:48:58 +0100

    Divisible By Three
    Divisible by three exercise.

---
+++ SoftUni JavaScript Fundamentals course - May 24/01-Basic Syntax, Conditional Statements and Loops/divisibleByThree-08.js
@@ -0,0 +1,15 @@
+function divisibleByThree() {
+
+    // for (let i = 1; i <= 100; i++) {
+    //     if (i % 3 === 0) {
+    //         console.log(i);
+    //     }
+    // }
+
+    for (let i = 3; i <= 100; i += 3) {
+        console.log(i);
+    }
+
+}
+
+divisibleByThree();
