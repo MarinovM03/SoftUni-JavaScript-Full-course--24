ec9f03ecaea9d1973f4762f04dbe4554f4a8b5f2
Martin Marinov <159044101+MarinovM03@users.noreply.github.com>
2024-10-05 17:55:12 +0100

    Sum Of Odd Numbers
    Sum of odd numbers exercise.

---
+++ SoftUni JavaScript Fundamentals course - May 24/01-Basic Syntax, Conditional Statements and Loops/sumOfOddNumbers-11.js
@@ -0,0 +1,12 @@
+function sumOfOddNumbers(n) {
+    let sum = 0;
+    
+    for (let i = 1; i <= n * 2 - 1; i += 2) {
+        console.log(i);
+        sum += i;
+    }
+
+    console.log(`Sum: ${sum}`);
+}
+
+sumOfOddNumbers(5);
