4689a8be916defa1ccd025283b4e48179a4172d3
Martin Marinov <159044101+MarinovM03@users.noreply.github.com>
2024-10-08 20:26:01 +0100

    Multiplication Table
    Multiplication table exercise.

---
+++ SoftUni JavaScript Fundamentals course - May 24/02-Execise: Basic Syntax, Conditional Statements and Loops/multiplicationTable-08.js
@@ -0,0 +1,11 @@
+function multiTable(num) {
+   
+    for (let i = 1; i <= 10; i++) {
+        let result = num * i;
+
+        console.log(`${num} X ${i} = ${result}`);
+    }
+}
+
+multiTable(5);
+// multiTable(2);
