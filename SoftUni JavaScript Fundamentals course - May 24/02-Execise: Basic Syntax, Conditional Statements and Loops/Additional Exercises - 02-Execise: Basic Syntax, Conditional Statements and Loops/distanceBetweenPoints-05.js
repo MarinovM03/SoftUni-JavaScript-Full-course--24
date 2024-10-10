2aa94e17ec86835680befd4ec818e9ce2d87bd60
Martin Marinov <159044101+MarinovM03@users.noreply.github.com>
2024-10-10 21:59:24 +0100

    Distance Between Points
    Distance between points exercise.

---
+++ SoftUni JavaScript Fundamentals course - May 24/02-Execise: Basic Syntax, Conditional Statements and Loops/Additional Exercises - 02-Execise: Basic Syntax, Conditional Statements and Loops/distanceBetweenPoints-05.js
@@ -0,0 +1,8 @@
+function distanceBetweenPoints(x1, y1, x2, y2) {
+ 
+    let distance = Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
+
+    console.log(distance);
+}
+
+distanceBetweenPoints(2.34,15.66,-13.55,-2.9985);
