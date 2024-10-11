959c3be0cf27fa093a9d0cd645cba72639ef1f1d
Martin Marinov <159044101+MarinovM03@users.noreply.github.com>
2024-10-11 23:51:22 +0100

    Concatenate Names
    Concatenate names exercise.

---
+++ SoftUni JavaScript Fundamentals course - May 24/03-Data Types and Variables/concatenateNames-02.js
@@ -0,0 +1,8 @@
+function concatenateNames(firstName, secondName, symbol) {
+
+    console.log(`${firstName}${symbol}${secondName}`);
+    
+}
+
+concatenateNames("John", "Smith", "->");
+concatenateNames("Jan", "White", "<->");
