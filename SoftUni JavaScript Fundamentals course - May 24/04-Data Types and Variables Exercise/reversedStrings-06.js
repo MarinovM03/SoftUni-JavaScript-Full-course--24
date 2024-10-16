8a783c90cafb3e4834ac41859b8a508528578684
Martin Marinov <159044101+MarinovM03@users.noreply.github.com>
2024-10-16 05:15:42 +0100

    Reversed Strings
    Reversed strings exercise.

---
+++ SoftUni JavaScript Fundamentals course - May 24/04-Data Types and Variables Exercise/reversedStrings-06.js
@@ -0,0 +1,13 @@
+function reversedStrings(str) {
+    let reversed = '';
+
+    for (let i = str.length - 1; i >= 0; i--) {
+        reversed += str[i];
+    }
+
+    console.log(reversed);
+}
+
+reversedStrings('Information');
+// reversedStrings('star');
+// reversedStrings('racecar');
