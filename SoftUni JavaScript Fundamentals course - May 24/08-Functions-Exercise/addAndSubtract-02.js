688d95c07608dce0acefef43228beabcdcbd357f
Martin Marinov <159044101+MarinovM03@users.noreply.github.com>
2024-11-09 04:50:00 +0000

    Add and Subtract
    Add and subtract exercise.

---
+++ SoftUni JavaScript Fundamentals course - May 24/08-Functions-Exercise/addAndSubtract-02.js
@@ -0,0 +1,17 @@
+function addAndSubtract(num1, num2, num3) {
+
+    function sum(x, y) {
+        return x + y;
+    }
+
+    function subtract(x, y) {
+        return x - y;
+    }
+
+    const firstNums = sum(num1, num2);
+    console.log(result);
+}
+
+addAndSubtract(23,6,10);
+// addAndSubtract(1,17,30);
+// addAndSubtract(42,58,100);
