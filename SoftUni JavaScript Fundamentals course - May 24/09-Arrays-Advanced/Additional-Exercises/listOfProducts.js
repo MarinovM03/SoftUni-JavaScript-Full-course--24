c6966a5770e7c83844d6fcba31d6a0423dec9acd
Martin Marinov <159044101+MarinovM03@users.noreply.github.com>
2024-11-19 21:51:29 +0000

    List of Products
    List of products exercise.

---
+++ SoftUni JavaScript Fundamentals course - May 24/09-Arrays-Advanced/Additional-Exercises/listOfProducts.js
@@ -0,0 +1,11 @@
+function listOfProducts(arr) {
+    
+    let sortedProducts = arr.sort();
+
+    for (let i = 0; i < sortedProducts.length; i++) {
+        console.log(`${i + 1}.${sortedProducts[i]}`);
+    }
+}
+
+listOfProducts(['Potatoes', 'Tomatoes', 'Onions', 'Apples']);
+// listOfProducts(['Watermelon', 'Banana', 'Apples']);
