4a3d86d547d6207e051488ca86b51fa645b87419
Martin Marinov <159044101+MarinovM03@users.noreply.github.com>
2025-01-11 21:22:20 +0000

    Substring
    Substring exercise.

---
+++ SoftUni JavaScript Fundamentals course - May 24/15-Text Processing/substring-02.js
@@ -0,0 +1,7 @@
+function substring(str, startIndex, count) {
+    const result = str.substring(startIndex, startIndex + count);
+    console.log(result);
+}
+
+substring('ASentence', 1, 8);
+// substring('SkipWord', 4, 7);
