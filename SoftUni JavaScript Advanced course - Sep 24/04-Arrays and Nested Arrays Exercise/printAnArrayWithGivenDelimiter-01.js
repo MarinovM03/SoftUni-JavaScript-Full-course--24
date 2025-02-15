1643103a2757dd6f92b57f41f7eaeb9cff6c2c49
Martin Marinov <159044101+MarinovM03@users.noreply.github.com>
2025-02-15 17:29:02 +0000

    Print an Array with Given Delimiter
    Print an array with given delimiter exercise.

---
+++ SoftUni JavaScript Advanced course - Sep 24/04-Arrays and Nested Arrays Exercise/printAnArrayWithGivenDelimiter-01.js
@@ -0,0 +1,6 @@
+function printAnArrWithGivenDelimiter(arr, delimiter) {
+    return arr.join(delimiter);
+}
+
+console.log(printAnArrWithGivenDelimiter(['One', 'Two', 'Three', 'Four', 'Five'], '-'));
+console.log(printAnArrWithGivenDelimiter(['How about no?', 'I','will', 'not', 'do', 'it!'], '_'));
