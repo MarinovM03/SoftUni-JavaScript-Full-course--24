56139a2f21e3348bc6aa556101a07d0120e8dedb
Martin Marinov <159044101+MarinovM03@users.noreply.github.com>
2025-02-25 06:08:54 +0000

    City Record
    City Record exercise.

---
+++ SoftUni JavaScript Advanced course - Sep 24/SoftUni JavaScript Advanced/05-Objects and Composition/cityRecord-01.js
@@ -0,0 +1,12 @@
+function cityRecord(name, population, treasury) {
+    const cityInfo = {
+        name,
+        population,
+        treasury
+    };
+
+    return cityInfo;
+}
+
+console.log(cityRecord('Tortuga',7000,15000));
+console.log(cityRecord('Santo Domingo',12000,23500));
