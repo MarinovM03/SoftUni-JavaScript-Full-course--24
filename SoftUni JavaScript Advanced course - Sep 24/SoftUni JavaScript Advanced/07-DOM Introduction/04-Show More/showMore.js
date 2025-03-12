af5f0a3ad6622056782b48643ff58eee43efa7e3
Martin Marinov <159044101+MarinovM03@users.noreply.github.com>
2025-03-12 03:30:38 +0000

    Show More
    Show more exercise.

---
+++ SoftUni JavaScript Advanced course - Sep 24/SoftUni JavaScript Advanced/07-DOM Introduction/04-Show More/showMore.js
@@ -0,0 +1,7 @@
+function showText() {
+    const aElement = document.getElementById('more');
+    const textElement = document.getElementById('text');
+
+    aElement.style.display = 'none';
+    textElement.style.display = 'inline';
+}
