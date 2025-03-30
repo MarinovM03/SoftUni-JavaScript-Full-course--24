781cd647e3bde11cbbb151a85f715397b5101893
Martin Marinov <159044101+MarinovM03@users.noreply.github.com>
2025-03-30 17:01:05 +0100

    Solution
    Solution exercise.

---
+++ SoftUni JavaScript Advanced course - Sep 24/SoftUni JavaScript Advanced/08-DOM Introduction Exercise/06-Format the Text/solution.js
@@ -0,0 +1,21 @@
+function solve() {
+    const inputEl = document.querySelector('#input');
+    const outputEl = document.querySelector('#output');
+
+    if (inputEl.value === '') return;
+
+    const sentences = inputEl.value.split('. ');
+    const sentencesPerParagraph = 3;
+
+    const numberOfParagraphs = Math.ceil(sentences.length / sentencesPerParagraph);
+
+    let output = '';
+
+    for (let i = 0; i < numberOfParagraphs; i++) {
+        const p = i * numberOfParagraphs;
+        const text = sentences.slice(p, (p + sentencesPerParagraph)).join('. ');
+        output += `<p>${text}</p>`;
+    }
+
+    outputEl.innerHTML = output;
+}
