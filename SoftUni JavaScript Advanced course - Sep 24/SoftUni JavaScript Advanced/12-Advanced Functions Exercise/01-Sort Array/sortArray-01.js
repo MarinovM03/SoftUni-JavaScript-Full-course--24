31a8972c186482897eec9626cd1b91ef6a60956c
Martin Marinov <martin.marinov10@yahoo.com>
2025-05-21 04:13:34 +0100

    Sort Array
    Sort array exercise.

---
+++ SoftUni JavaScript Advanced course - Sep 24/SoftUni JavaScript Advanced/12-Advanced Functions Exercise/01-Sort Array/sortArray-01.js
@@ -0,0 +1,6 @@
+function solve(arr, operation) {
+    return operation === 'asc' ? arr.sort((a, b) => a - b) : arr.sort((a, b) => b - a);
+}
+
+console.log(solve([14, 7, 17, 6, 8], 'asc'));
+console.log(solve([14, 7, 17, 6, 8], 'desc'));
