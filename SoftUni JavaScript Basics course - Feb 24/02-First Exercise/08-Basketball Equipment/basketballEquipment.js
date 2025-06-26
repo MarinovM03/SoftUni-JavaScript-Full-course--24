cc93278503f124730954abe10daff9a240c1de68
Martin Marinov <martin.marinov10@yahoo.com>
2025-06-26 06:06:44 +0100

    Basketball Equipment
    Basketball Equipment exercise uploaded.

---
+++ SoftUni JavaScript Basics course - Feb 24/02-First Exercise/08-Basketball Equipment/basketballEquipment.js
@@ -0,0 +1,19 @@
+function basketballEquipment(tax) {
+    tax = Number(tax);
+
+    let shoes = tax * 0.40;
+    let kit = shoes * 0.20;
+    let ball = kit * 0.25;
+    let accessories = ball * 0.20;
+
+    let shoesSum = tax - shoes;
+    let kitSum = tax - (shoesSum - kit);
+    let ballSum = kitSum * 0.25;
+    let accessoriesSum = ballSum * 0.20;
+
+    let totalSum = tax + shoesSum + kitSum + ballSum + accessoriesSum;
+
+    console.log(totalSum);
+}
+
+basketballEquipment(5);
