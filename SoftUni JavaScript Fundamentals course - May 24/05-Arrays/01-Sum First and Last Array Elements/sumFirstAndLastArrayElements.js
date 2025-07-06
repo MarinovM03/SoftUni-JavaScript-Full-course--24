f984baf649be2ffc6a9996e7d31e3a7b3742d5fd
Martin Marinov <martin.marinov10@yahoo.com>
2025-07-06 12:53:58 +0100

    Sum First and Last Array Elements
    Sum First and Last Array Elements exercise uploaded.

---
+++ SoftUni JavaScript Fundamentals course - May 24/05-Arrays/01-Sum First and Last Array Elements/sumFirstAndLastArrayElements.js
@@ -0,0 +1,12 @@
+function sumFirstNLastArrElements(nums) {
+
+    let firstElementFromArray = nums[0];
+    let lastElementFromArray = nums[nums.length - 1];
+
+    console.log(firstElementFromArray + lastElementFromArray);
+}
+
+sumFirstNLastArrElements([20, 30, 40]);
+// sumFirstNLastArrElements([10, 17, 22, 33]);
+// sumFirstNLastArrElements([11, 58, 69]);
+
